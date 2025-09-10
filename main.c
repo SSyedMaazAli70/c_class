6381754cb9793d9fa51d41234c74fe7709f9c89e
SSyedMaazAli70 <ssyedmaazali70@gmail.com>
2025-09-10 11:13:21 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+// ID: 202512440
+// Name: Syed Maaz Ali Shah
+
+#include <stdio.h>
+
+int main(void) {
+    // Write your code here to display "Hello, world!"
+    printf("hello, world\n");
+    return 0;
+}
